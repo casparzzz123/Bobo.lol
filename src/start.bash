f15e410227e7129f24e14c3ddf4b60e345031805
casparzzz123 <80700684+casparzzz123@users.noreply.github.com>
2022-08-08 21:29:38 +0200

    Add files via upload

---
+++ src/start.bash
@@ -0,0 +1,7 @@
+TODO:
+
+    - npm init
+    - npm install tailwindcss - https://tailwindcss.com/docs/installation
+    - npm install express - https://expressjs.com/
+    - npm install ping-lite
+    - npm i react-snowfetti
